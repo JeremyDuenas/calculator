422874d41b49adc6b9b85e0b58d4aab6e915e7d0
JeremyDuenas <ssjjhd@gmail.com>
2017-04-05 22:13:03 -0700

    testing VS for Mac

---
+++ calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace calculator
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int input1;
+			int input2;
+			int sum;
+
+			Console.WriteLine("Enter a number:");
+			input1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter another number:");
+			input2 = Convert.ToInt32(Console.ReadLine());
+			sum = input1 + input2;
+			Console.WriteLine("The sum of these two numbers is: {0}", sum);
+
+		}
+	}
+}
